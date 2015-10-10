ca86b54c2a77282d11d690219c0273b97569817f
BootCoder <bootcoder@gmail.com>
2015-10-09 17:58:23 -0700

    add new user form - has_secure_pw

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    p @user
+    redirect_to root_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:password, :password_confirmation)
+  end
+
 end
