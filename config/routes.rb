aa45eb9cf5a396d6e174d29451c40d1dae8c98bd
BootCoder <bootcoder@gmail.com>
2015-10-09 17:28:55 -0700

    add resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
+  resources :users
+  resources :meetups
+  resources :events
+  resources :slides
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
