9808ea30f2ce761d54a7f27600f830c2fdba8bc7
BootCoder <bootcoder@gmail.com>
2015-10-09 17:15:18 -0700

    g user/event/slide/meetup

---
+++ PSEUDO.rb
@@ -0,0 +1,10 @@
+# as a user I want to be able to log in
+# as a user I want to be able to log out
+# as a user I want to be able to control what is displayed
+# - image slides
+# - schedule of events
+# as a user I want to see events that are happening today
+# as a user I want to see events that are happening this week
+# as a user I want to see events that are happening this week
+# as a user I want to see meet ups in the space
+# as a user I want to
