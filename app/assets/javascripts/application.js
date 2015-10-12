b54e7448651a9b94fffecbaf0c25e1cedf874dfa
BootCoder <bootcoder@gmail.com>
2015-10-11 17:05:51 -0700

    install react

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
