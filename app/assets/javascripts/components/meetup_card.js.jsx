a8ecb24c0d45ffc98902b216c0b8779485db7287
Tarik Bennett <tarik.bennett@gmail.com>
2015-10-14 19:25:26 -0700

    Update event card information - schedule,img,description. Add styling classes

--- app/assets/javascripts/components/meetup_card.js.jsx
+++ app/assets/javascripts/components/meetup_card.js.jsx
@@ -3,10 +3,18 @@ var MeetupCard = React.createClass({
   render: function() {
     return (
       <div>
-        {this.props.event.id}
-        {this.props.event.organizer}
-        {this.props.event.location}
-
+        <div className="card-layout">
+          {this.props.event.id}
+          {this.props.event.schedule}
+          <div className="content-right">
+            {this.props.event.organizer}
+            {this.props.event.location}
+            {this.props.event.description}
+          </div>
+          <div className="image-left">
+            <img src={this.props.event.img_url} />
+          </div>
+        </div>
       </div>
       );
   }
