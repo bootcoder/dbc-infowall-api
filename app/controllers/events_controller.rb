1dabe4bb75acdb9c93e3c2de6dd9f46b1c922205
BootCoder <bootcoder@gmail.com>
2015-10-15 17:19:45 -0700

    update event_con with respond to - mod CSRF auth

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,53 +1,89 @@
 class EventsController < ApplicationController
 
   EVENT_NOT_FOUND_ERROR = { :errors => "ERROR: Event not found." }
+  EVENT_UPDATE_ERROR = { :errors => "ERROR: Event update failed." }
 
   def index
     @events = Event.all_meetups
-    ap @events
-    @events = Event.all
-    render json: @events
+   respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @events }
+    end
   end
+
 
   def new
     @event = Event.new
-    render json: @event
+    respond_to do |format|
+      format.html { render :new }
+      format.json { render json: @event }
+    end
   end
 
   def show
     @event = Event.find_by_id(params[:id])
     if @event
-      render json: @event
+      respond_to do |format|
+        format.html { render :show }
+        format.json { render json: @event }
+      end
     else
-      render json: EVENT_NOT_FOUND_ERROR
+      respond_to do |format|
+        format.html { render :show }
+        format.json { render json: EVENT_NOT_FOUND_ERROR }
+      end
     end
   end
 
   def edit
     @event = Event.find_by_id(params[:id])
     if @event
-      render json: @event
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: @event }
+      end
     else
-      render json: EVENT_NOT_FOUND_ERROR
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: EVENT_NOT_FOUND_ERROR }
+      end
     end
   end
 
   def create
     @event = Event.new(event_params)
     if @event.save
-      render json: @event
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: @event }
+      end
     else
-      render json: @event.errors
+      respond_to do |format|
+        format.html { render :new  }
+        format.json { render json: @events.errors }
+      end
     end
   end
 
   def update
     @event = Event.find_by_id(params[:id])
     if @event
-      @event.update_attributes(event_params)
-      render json: @event
+      if @event.update_attributes(event_params)
+        respond_to do |format|
+          format.html { render :index }
+          format.json { render json: @events }
+        end
+      else
+        respond_to do |format|
+          format.html { render :edit }
+          format.json { render json: EVENT_UPDATE_ERROR }
+        end
+      end
     else
-      render json: EVENT_NOT_FOUND_ERROR
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: EVENT_NOT_FOUND_ERROR }
+      end
     end
   end
 
@@ -56,9 +92,15 @@ class EventsController < ApplicationController
     if @event
       @event.destroy
       @events = Event.all
-      render json: @events
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: @events }
+      end
     else
-      render json: EVENT_NOT_FOUND_ERROR
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: EVENT_NOT_FOUND_ERROR }
+      end
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'users#index'
+  root 'events#index'
 
   get  'login'  => 'sessions#new'
   post 'login' => 'sessions#create'
