74eb6a71d6bbee4c37a878c9b850d059e2e63333
BootCoder <bootcoder@gmail.com>
2015-10-16 01:31:39 -0700

    uniform cards - solid header

--- app/assets/javascripts/components/meetup_card.js.jsx
+++ app/assets/javascripts/components/meetup_card.js.jsx
@@ -4,18 +4,18 @@ var MeetupCard = React.createClass({
     return (
       <div className="small-3 large-3 columns margin-1 fullWidth">
         <div className="sb-dark gray-text height-200px row relative-div">
-          <p className="center height-50px bold">{this.props.event.title}</p>
+          <p className="center white-text vertical-center fullWidth pad-top-5px height-50px bold">{this.props.event.title}</p>
           <div className="center small-12 large-12 columns">
             <p className="desc-text">{this.props.event.description}</p>
           </div>
-          <div className="small-12 large-12 bg-teal absolute-bottom height-60px">
+          <div className="small-12 large-12 bg-teal absolute-bottom height-50px">
             <div className="row">
               <div className="large-9 columns white-text no-margin absolute-bottom">
-                <p className="bold date-text">{formatDate(this.props.event.schedule)}</p>
-                <p className="bold date-text">{this.props.event.location}</p>
+                <p className="bold pad-left-35px date-text">{formatDate(this.props.event.schedule)}</p>
+                <p className="bold pad-left-30px date-text">{this.props.event.location}</p>
               </div>
               <div className="large-3 columns no-pad">
-                <Img className=" fullWidth" src={this.props.event.img_url} className ="height-60px right" />
+                <Img className=" fullWidth" src={this.props.event.img_url} className ="square-50px right" />
               </div>
             </div>
           </div>
