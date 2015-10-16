d8748b90740fad18c2ce86a4f8c21e673184d79e
BootCoder <bootcoder@gmail.com>
2015-10-16 05:32:07 -0700

    update image to circle

--- app/assets/javascripts/components/meetup_image.js.jsx
+++ app/assets/javascripts/components/meetup_image.js.jsx
@@ -3,7 +3,7 @@ var MeetupImage = React.createClass({
   render: function() {
     return (
       <div className="small-3 large-3 columns no-pad-left">
-        <Img className="fullWidth" src={this.props.event.img_url} className ="square-60px right" />
+        <Img className="fullWidth" src={this.props.event.img_url} className ="circle-60px right" />
       </div>
     );
   }
