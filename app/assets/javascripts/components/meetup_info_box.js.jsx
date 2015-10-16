25da44d221d9e4ee9facfd407eb27c05db6744db
BootCoder <bootcoder@gmail.com>
2015-10-16 05:17:20 -0700

    add initial color coding for cards

--- app/assets/javascripts/components/meetup_info_box.js.jsx
+++ app/assets/javascripts/components/meetup_info_box.js.jsx
@@ -2,13 +2,24 @@ var MeetupInfoBox = React.createClass({
 
   render: function() {
     var props = this.props.event
-    return (
-      <div className="small-12 large-12 bg-teal absolute-bottom height-60px">
-        <div className="row">
-          <MeetupDate event={props} />
-          <MeetupImage event={props} />
+    if (props.event_type == 'student'){
+      return (
+        <div className="small-12 large-12 bg-card absolute-bottom height-60px">
+          <div className="row">
+            <MeetupDate event={props} />
+            <MeetupImage event={props} />
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else if (props.event_type == 'meetup') {
+      return (
+        <div className="small-12 large-12 bg-teal absolute-bottom height-60px">
+          <div className="row">
+            <MeetupDate event={props} />
+            <MeetupImage event={props} />
+          </div>
+        </div>
+      );
+    }
   }
 });
