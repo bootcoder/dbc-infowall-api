2c00772dd75c170e21036476e87a5f45ca62423f
BootCoder <bootcoder@gmail.com>
2015-10-16 05:41:40 -0700

    round edge of card near pic

--- app/assets/javascripts/components/meetup_card.js.jsx
+++ app/assets/javascripts/components/meetup_card.js.jsx
@@ -4,7 +4,7 @@ var MeetupCard = React.createClass({
     var props = this.props.event
     return (
       <div className="small-12 large-3 columns margin-1 fullWidth">
-        <div className="sb-dark gray-text height-300px row relative-div rounded-corners">
+        <div className="sb-dark gray-text height-300px row relative-div rounded-corners rounded-bottom">
           <MeetupTitle event={props} />
           <MeetupDesc event={props} />
           <MeetupInfoBox event={props} />
--- app/assets/javascripts/components/meetup_info_box.js.jsx
+++ app/assets/javascripts/components/meetup_info_box.js.jsx
@@ -4,7 +4,7 @@ var MeetupInfoBox = React.createClass({
     var props = this.props.event
     if (props.event_type == 'student'){
       return (
-        <div className="small-12 large-12 bg-gray absolute-bottom height-60px">
+        <div className="small-12 large-12 bg-gray absolute-bottom height-60px rounded-bottom">
           <div className="row">
             <MeetupDate event={props} />
             <MeetupImage event={props} />
@@ -13,7 +13,7 @@ var MeetupInfoBox = React.createClass({
       );
     } else if (props.event_type == 'meetup') {
       return (
-        <div className="small-12 large-12 bg-teal absolute-bottom height-60px">
+        <div className="small-12 large-12 bg-teal absolute-bottom height-60px rounded-bottom">
           <div className="row">
             <MeetupDate event={props} />
             <MeetupImage event={props} />
