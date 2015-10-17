d16b50c1550948d9da1f4d38508810c8a8816905
BootCoder <bootcoder@gmail.com>
2015-10-17 13:30:35 -0700

    Meetup components => Card components

--- app/assets/javascripts/components/meetup_title.js.jsx
+++
@@ -1,8 +0,0 @@
-var MeetupTitle = React.createClass({
-
-  render: function() {
-    return (
-      <p className="center teal-text title-text fullWidth pad-top-10px height-50px bold">{this.props.event.title}</p>
-      );
-  }
-});
