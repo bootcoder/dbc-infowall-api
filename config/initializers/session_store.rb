e2f7f1d7a3ca9b5afdeefc5c238a2f1a9cdf022f
BootCoder <bootcoder@gmail.com>
2015-10-24 02:13:05 -0700

    play with csrf

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_full-foyer-rails_session', domain: 'my_app.com'
+Rails.application.config.session_store :cookie_store,
+                                        key: '_full-foyer-rails_session'
+
