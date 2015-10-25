dc2b8a8656b3b7bf4bf3e838c48832f1bac1b696
BootCoder <bootcoder@gmail.com>
2015-10-24 19:28:55 -0700

    omni config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,18 @@
 #config/initalizers/omniauth.rb
+
+OmniAuth.config.full_host = Rails.env.production? ? 'https://dbc-infowall.herokuapp.com' : 'http://localhost:3000'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-
-  OmniAuth.config.full_host = Rails.env.production? ? 'https://dbc-infowall.herokuapp.com' : 'http://localhost:3000'
-
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-  scope: ['email',
-    'https://www.googleapis.com/auth/calendar.readonly']}
+    scope: ['email',
+      'https://www.googleapis.com/auth/gmail.modify','https://www.googleapis.com/auth/userinfo.email'],
+    access_type: 'offline',
+    :provider_ignores_state => true,
+    :prompt => "select_account",
+    setup: (lambda do |env|
+      request = Rack::Request.new(env)
+      env['omniauth.strategy'].options['token_params'] = {:redirect_uri => 'https://dbc-infowall.herokuapp.com/auth/google_oauth2/callback'}
+    end)
+    }
+
 end
