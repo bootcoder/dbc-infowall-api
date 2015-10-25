3a5a6f79f1881e3a2033587c004ca2b71c499997
BootCoder <bootcoder@gmail.com>
2015-10-24 19:26:18 -0700

    add omni config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 #config/initalizers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
+
+  OmniAuth.config.full_host = Rails.env.production? ? 'https://dbc-infowall.herokuapp.com' : 'http://localhost:3000'
+
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
   scope: ['email',
     'https://www.googleapis.com/auth/calendar.readonly']}
