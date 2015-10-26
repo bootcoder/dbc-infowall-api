6ca4fa770d9daec78e743c13e991b83360adc61a
BootCoder <bootcoder@gmail.com>
2015-10-25 20:37:18 -0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025024119) do
+ActiveRecord::Schema.define(version: 20151026032500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,14 +59,19 @@ ActiveRecord::Schema.define(version: 20151025024119) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tokens", force: :cascade do |t|
+    t.string   "access_token"
+    t.string   "refresh_token"
+    t.datetime "expires_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "access_token"
-    t.string   "refresh_token"
-    t.datetime "expires_at"
   end
 
 end
