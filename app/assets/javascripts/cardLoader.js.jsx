3020fe4a9a0a1447ac99bd1c9f705b8b0570acee
BootCoder <bootcoder@gmail.com>
2015-10-30 10:38:49 -0700

    update slide transition time

--- app/assets/javascripts/cardLoader.js.jsx
+++ app/assets/javascripts/cardLoader.js.jsx
@@ -48,6 +48,6 @@ var getCardsIf = function(counter){
   } else if (window.location.pathname === "/meetups/cards"){
     getMeetups();
   } else if (window.location.pathname === "/cards" || window.location.pathname === '/') {
-    setTimeout(function(){ cycleCardView(counter); }, 20000);
+    setTimeout(function(){ cycleCardView(counter); }, 35000);
   }
 }
