53a4ad040b9cd67f1064ae16b57c55aed7462cad
Hunter Chapman <bootcoder@gmail.com>
2015-11-14 20:14:03 -0800

    clean calendar controller - chg event type to calendar on import

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,16 +2,7 @@ class CalendarsController < ApplicationController
 
   def index
     @calendar = Calendar.new(Token.last)
-    p "% " * 200
-    ap @calendar
-    p "% " * 200
-    ap @calendar.cal
-    p "% " * 200
-    ap @calendar.test
-    # p @calendar.event
-    # p @calendar.find_event
-    p "- " * 200
     @calendar.import_events
-    p "- " * 200
+    @calendar_events = Event.where(event_type: "calendar")
   end
 end
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -23,35 +23,6 @@ class Calendar
     @cal
   end
 
-  def some_flow
-    puts "Do you already have a refresh token? (y/n)"
-    has_token = $stdin.gets.chomp
-
-    if has_token.downcase != 'y'
-
-      # A user needs to approve access in order to work with their @calendars.
-      puts "Visit the following web page in your browser and approve access."
-      puts @cal.authorize_url
-      puts "\nCopy the code that Google returned and paste it here:"
-
-      # Pass the ONE TIME USE access code here to login and get a refresh token that you can use for access from now on.
-      refresh_token = @cal.login_with_auth_code( $stdin.gets.chomp )
-
-      puts "\nMake sure you SAVE YOUR REFRESH TOKEN so you don't have to prompt the user to approve access again."
-      puts "your refresh token is:\n\t#{refresh_token}\n"
-      puts "Press return to continue"
-      $stdin.gets.chomp
-
-    else
-
-      puts "Enter your refresh token"
-      refresh_token = $stdin.gets.chomp
-      @cal.login_with_refresh_token(refresh_token)
-
-      # Note: You can also pass your refresh_token to the constructor and it will login at that time.
-
-    end
-  end
   def event
     # @event = @cal.create_event
     @event = @cal.create_event do |e|
@@ -61,6 +32,7 @@ class Calendar
     end
     @event.save
   end
+
   def find_event
     @event = @cal.find_or_create_event_by_id(@event.id) do |e|
       e.title = 'An Updated Cool Event'
@@ -107,16 +79,14 @@ class Calendar
                       organizer: event.creator_name,
                       location: event.location,
                       img_url: get_img_url(event.creator_name),
-                      event_type: "student",
+                      event_type: "calendar",
                       attending: 0,
                       schedule: DateTime.parse(event.raw['start']['dateTime'])
                       )
       ap @event
       ap @event.errors
+      @event
     end
   end
-
-  # Query events
-  # puts @cal.find_events('your search string')
 
 end
