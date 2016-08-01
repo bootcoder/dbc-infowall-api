26c52a60af19fa763dd10096afa69c4dbfeac5ad
bootcoder <bootcoder@gmail.com>
2016-08-01 10:19:08 -0700

    HOTFIX -> Destroy all Mentors on Mentor refresh - dont display non-floor mentors

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,13 +1,16 @@
 class Mentor < ActiveRecord::Base
 
   def self.parse_deputy_mentors(deputy_array)
-    return nil if deputy_array == nil || deputy_array.empty?
+    Mentor.destroy_all
+    return nil if deputy_array == (nil || deputy_array.empty?)
     results = []
     deputy_array.each do |mentor|
+      phase = mentor["_DPMetaData"]["OperationalUnitInfo"]["OperationalUnitName"]
+      next if phase == ("Admin" || "Remote Support" || "Interviews" || "Phase 0 Guide")
       results << Mentor.create(
         name: mentor["_DPMetaData"]["EmployeeInfo"]["DisplayName"],
         img_url: mentor["_DPMetaData"]["EmployeeInfo"]["Photo"],
-        phase: mentor["_DPMetaData"]["OperationalUnitInfo"]["OperationalUnitName"])
+        phase: phase)
     end
     results.sort_by { |mentor| mentor.phase }
   end
