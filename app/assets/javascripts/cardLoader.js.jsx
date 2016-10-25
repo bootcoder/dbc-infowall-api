c3c31fd9f348a5367ae69e0281cfc2fc9b534ccc
BootCoder <bootcoder@gmail.com>
2016-10-24 21:38:09 -0700

    standard refactor - cardLoader.js.jsx

--- app/assets/javascripts/cardLoader.js.jsx
+++ app/assets/javascripts/cardLoader.js.jsx
@@ -1,76 +1,75 @@
-
-var renderReactEvents = function(data){
-  React.render(<EventCardContainer events={data}/>, document.getElementById('content'));
+var renderReactEvents = function (data) {
+  React.render(<EventCardContainer events={data}/>, document.getElementById('content'))
 }
 
-var renderReactPeople = function(data){
-  React.render(<PersonCardContainer people={data}/>, document.getElementById('content'));
+var renderReactPeople = function (data) {
+  React.render(<PersonCardContainer people={data}/>, document.getElementById('content'))
 }
 
-var getEvents = function(){
-  $.get( "/events/cards.json", function( data ) {
-    if(data === null || data === undefined || data.length == 0){ return getCalendar(); };
-    renderReactEvents(data);
-  });
+var getEvents = function () {
+  $.get('/events/cards.json', function( data ) {
+    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+    renderReactEvents(data)
+  })
 }
 
-var getMentors = function(){
-  $.get( "/mentors.json", function( data ) {
-    if(data === null || data === undefined || data.length == 0){ return getCalendar(); };
-    renderReactPeople(data);
-  });
+var getMentors = function () {
+  $.get('/mentors.json', function (data) {
+    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+    renderReactPeople(data)
+  })
 }
 
-var getStaff = function(){
-  $.get( "/staff.json", function( data ) {
-    if(data === null || data === undefined || data.length == 0){ return getCalendar(); };
-    renderReactPeople(data);
-  });
+var getStaff = function () {
+  $.get('/staff.json', function (data) {
+    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+    renderReactPeople(data)
+  })
 }
 
-var getToday = function(){
-  $.get( "/cards.json", function( data ) {
+var getToday = function () {
+  $.get('/cards.json', function (data) {
     console.log(data)
-    if(data === null || data === undefined || data.length == 0){ return getCalendar(); };
-    renderReactEvents(data);
-  });
+    if (data === null || data === undefined || data.length === 0) { return getCalendar() }
+    renderReactEvents(data)
+  })
 }
 
-var getCalendar = function(){
-  $.get( "/calendars.json", function( data ) {
-    renderReactEvents(data);
-  });
+var getCalendar = function () {
+  $.get( '/calendars.json', function (data) {
+    renderReactEvents(data)
+  })
 }
 
-var cycleCardView = function(counter){
-  console.log(counter);
+var cycleCardView = function (counter) {
+  console.log(counter)
   if (counter === 0) {
-    counter += 1;
-    getToday();
-    getCardsIf(counter);
+    counter += 1
+    getToday()
+    getCardsIf(counter)
   } else if (counter === 1) {
-    counter += 1;
-    getStaff();
-    getCardsIf(counter);
+    counter += 1
+    getStaff()
+    getCardsIf(counter)
   } else if (counter === 2) {
-    counter += 1;
-    getMentors();
-    getCardsIf(counter);
+    counter += 1
+    getMentors()
+    getCardsIf(counter)
   } else if (counter === 3) {
-    counter = 0;
-    getCalendar();
-    getCardsIf(counter);
+    counter = 0
+    getCalendar()
+    getCardsIf(counter)
   }
 }
 
-var getCardsIf = function(counter){
-  if (window.location.pathname === "/events/cards"){
-    getEvents();
-  } else if (window.location.pathname === "/mentors"){
-    getMentors();
-  } else if (window.location.pathname === "/staff"){
-    getStaff();
-  } else if (window.location.pathname === "/cards" || window.location.pathname === '/') {
-    setTimeout(function(){ cycleCardView(counter); }, 40000);
+var getCardsIf = function (counter) {
+  if (window.location.pathname === '/events/cards') {
+    getEvents()
+  } else if (window.location.pathname === '/mentors') {
+    getMentors()
+  } else if (window.location.pathname === '/staff') {
+    getStaff()
+  } else if (window.location.pathname === '/cards' || window.location.pathname === '/') {
+    setTimeout(function () { cycleCardView(counter) }, 40000)
   }
 }
