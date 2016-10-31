6d77d25342e12c907e8568e00eac1039836fa882
BootCoder <bootcoder@gmail.com>
2016-10-31 13:47:21 -0700

    add dotenv gem - seed token

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Token.create(access_token: ENV["SEED_ACCESS_TOKEN"],
+             refresh_token: ENV["SEED_REFRESH_TOKEN"],
+             expires_at: "Thu, 07 Jul 2016 12:23:59 PDT -07:00")
