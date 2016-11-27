9eafd8289144ad194fa727c15d93d65372b8fd1f
BootCoder <bootcoder@gmail.com>
2016-11-26 20:15:22 -0800

    limit all results to 8 cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,25 +2,25 @@ class CardsController < ApplicationController
 
   def events
     @calendar_adapter = CalendarAdapter.new
-    @events = @calendar_adapter.parse_all_events
+    @events = @calendar_adapter.parse_all_events[0..7]
     render json: @events
   end
 
   def today
     @calendar_adapter = CalendarAdapter.new
-    @events = @calendar_adapter.events_today
+    @events = @calendar_adapter.events_today[0..7]
     render json: @events
   end
 
   def mentors
     @deputy_adapter = DeputyAdapter.new
-    @mentors = @deputy_adapter.mentors_on_shift
+    @mentors = @deputy_adapter.mentors_on_shift[0..7]
     render json: @mentors
   end
 
   def workshops
     @deputy_adapter = DeputyAdapter.new
-    @workshops = @deputy_adapter.list_upcoming_workshops
+    @workshops = @deputy_adapter.list_upcoming_workshops[0..7]
     render json: @workshops
   end
 
@@ -29,7 +29,7 @@ class CardsController < ApplicationController
 
   def staff
     @sheets_adapter = GoogleSheetsAdapter.new
-    @results = @sheets_adapter.get_daily_staff
+    @results = @sheets_adapter.get_daily_staff[0..7]
     ap @results
     render json: @results
   end
