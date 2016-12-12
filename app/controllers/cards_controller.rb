4fb96276419095f45cb8bca8c7e8ab02c09f99e0
BootCoder <bootcoder@gmail.com>
2016-12-12 10:25:43 -0800

    standardize return vars in cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,35 +2,38 @@ class CardsController < ApplicationController
 
   def events
     @calendar_adapter = CalendarAdapter.new
-    @events = @calendar_adapter.parse_all_events[0..7]
-    render json: @events
+    @results = @calendar_adapter.parse_all_events[0..7]
+    render json: @results
   end
 
   def today
     @calendar_adapter = CalendarAdapter.new
-    @events = @calendar_adapter.events_today[0..7]
-    render json: @events
+    @results = @calendar_adapter.events_today[0..7]
+    render json: @results
   end
 
   def mentors
     @deputy_adapter = DeputyAdapter.new
-    @mentors = @deputy_adapter.mentors_on_shift[0..7]
-    render json: @mentors
+    @results = @deputy_adapter.mentors_on_shift[0..7]
+    render json: @results
   end
 
   def workshops
     @deputy_adapter = DeputyAdapter.new
-    @workshops = @deputy_adapter.list_upcoming_workshops[0..7]
-    render json: @workshops
+    @results = @deputy_adapter.list_upcoming_workshops[0..7]
+    render json: @results
   end
 
   def topics
+    @sheets_adapter = GoogleSheetsAdapter.new
+    @results = @sheets_adapter.get_daily_topics[0..7]
+    render json: @results
   end
 
   def staff
     @sheets_adapter = GoogleSheetsAdapter.new
     @results = @sheets_adapter.get_daily_staff[0..7]
-    ap @results
+    # ap @results
     render json: @results
   end
 
