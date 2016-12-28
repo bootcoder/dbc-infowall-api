4106dcc3d6699167e92604d9af240f6face89ff9
BootCoder <bootcoder@gmail.com>
2016-12-28 00:14:52 -0500

    MVP staff payload

--- app/models/google_sheets_adapter.rb
+++ app/models/google_sheets_adapter.rb
@@ -1,6 +1,7 @@
 class GoogleSheetsAdapter
 
   # 2016 Lead for the day Schedule - DBC-SF
+  # Infowall Staff tab
   # https://docs.google.com/spreadsheets/d/1cNslAPoctBreV5dzNspIAbVckaY5RvaET81cl-8bJkM/edit#gid=2102156371
 
   def initialize
@@ -11,7 +12,7 @@ class GoogleSheetsAdapter
 
   def leads_worksheet
     @ws.each do |sheet|
-      return sheet if sheet.title.include?("CURRENT")
+      return sheet if sheet.title.include?("Infowall Staff")
     end
   end
 
@@ -22,7 +23,6 @@ class GoogleSheetsAdapter
   end
 
   def get_daily_topics
-    p "HERE " * 90
     results = {}
     @topics_worksheet ||= topics_worksheet
     ap @topics_worksheet.cells
@@ -30,33 +30,43 @@ class GoogleSheetsAdapter
   end
 
   def get_daily_staff
+
     # will need documentation in the schedule about how this pull works.
+    # Scratch that, decided to create a standalone sheet. FAQit.
+
     # first val == row
     # second val == col
-    # byebug
+
     results = {}
+    results['Walker'] = 'Campus Director'
+
     @leads_worksheet ||= leads_worksheet
-    ap @leads_worksheet.cells
-    # day_root_cell = find_day_root_cell
-    # return results if day_root_cell.nil?
-    # off_root_cell = find_off_root_cell(day_root_cell)
-    # count = off_root_cell[0] - day_root_cell[0]
-    #
-    # count.times do |count_int|
-    #   day_type = ws[[day_root_cell[0] + count_int, 2]]
-    #   day_lead = ws[[day_root_cell[0] + count_int, day_root_cell[1]]]
-    #   next if (day_type.nil? || day_lead.nil? || day_lead.match(/Final/))
-    #   results[day_lead.match(/^[^\W]*/).to_s] = day_type
-    # end
-    # results['Walker'] = 'Campus Director'
+    day_root_cell = find_day_root_cell
+    return results if day_root_cell.nil?
+    day_end_cell = find_day_end_cell(day_root_cell)
+    count = day_end_cell[0] - day_root_cell[0]
+
+    count.times do |count_int|
+      day_type = @leads_worksheet.cells[[day_root_cell[0] + count_int, 1]]
+      day_lead = @leads_worksheet.cells[[day_root_cell[0] + count_int, day_root_cell[1]]]
+      next if (day_type.nil? || day_lead.nil? || day_lead.match(/Final/) || day_type.match(/Week/))
+      results[day_lead.match(/^[^\W]*/).to_s] = day_type
+    end
+    p "in set staff " * 10
+    ap results
     results
+
   end
 
   def find_day_root_cell
-    today = Date.today.to_s
-    # today = Date.parse("12-08-2016").to_s
-    leads_worksheet.each do |key, value|
-      if value.match(/^\d{1,2}\/\d{1,2}\/\d{4}$/)
+
+    # Toggle this date on for debugging / DD/MM/YYYY format
+    today = Date.parse("2-1-2016").to_s
+    # today = Date.today.to_s
+
+    @leads_worksheet.cells.each do |key, value|
+      if value.match(/^\d{1,2}\/\d{1,2}$/)
+        value += "/#{Date.today.year}" unless value.match(/\d{4}/)
         formatted_value = Date.strptime(value, "%m/%d/%Y").to_s
         return key if Date.parse(formatted_value).to_s == today
       end
@@ -64,12 +74,11 @@ class GoogleSheetsAdapter
     nil
   end
 
-  def find_off_root_cell(day_root_cell)
-    root_cell = [day_root_cell[0], 2]
-
+  def find_day_end_cell(day_root_cell)
+    root_cell = [day_root_cell[0], 1]
     10.times do
       root_cell[0] += 1
-      return root_cell if leads_worksheet[root_cell] == "Off"
+      return root_cell if @leads_worksheet.cells[root_cell] == "End of Week"
     end
   end
 
--- app/models/person_card.rb
+++ app/models/person_card.rb
@@ -1,2 +1,15 @@
 class PersonCard < ApplicationRecord
+
+  def self.parse_staff_cards(card_hash)
+    results = []
+    card_hash.each do |person, role|
+      count = 0
+      results << PersonCard.new(name: person,
+                                phase: role,
+                                img_url: 'http://static.bootic.com/_pictures/1623251/batman-i-am-batman-flip-up_1.jpg',
+                                staff_type: 'staff',
+                                id: count += 1)
+    end
+    results
+  end
 end
